512bd6bf44dd8e83df2045ea0f0015264c84f642
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2023-07-02 17:37:47 +0800

    OG Fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <meta property='og:url' content='https://missbed.narix.link' />
+        <meta property='og:title' content='Missbed' />
+        <meta property='og:description' content='An embedding solution for Misskey' />
+        <meta property='og:image' content='https://missbed.narix.link/og.png' />
+      </Head>
       <body>
         <Main />
         <NextScript />
