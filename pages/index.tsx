49974b6cffd93121f9e5cf5d79fdf7ae21b00094
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2023-07-19 22:27:09 +0800

    Div100vh Deprecated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import dynamic from 'next/dynamic'
 import { Sawarabi_Mincho } from 'next/font/google'
 import Head from 'next/head'
-import Div100vh from 'react-div-100vh'
 import GithubCorner from 'react-github-corner'
 import Image from 'next/image'
 
@@ -12,7 +11,7 @@ const mincho = Sawarabi_Mincho({ weight: '400', subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <Div100vh className={`${mincho.className} bg-gradient-to-tr from-indigo-200 via-red-200 to-yellow-100 overflow-y-auto p-8`}>
+    <main className={`${mincho.className} bg-gradient-to-tr from-indigo-200 via-red-200 to-yellow-100 overflow-y-auto p-8`}>
       <Head>
         <meta name='description' content={'Embedding solution for Misskey. Homepage of Missbed.'} />
       </Head>
@@ -27,16 +26,16 @@ export default function Home() {
       <p className='text-center text-slate-500 italic'>Embedding solution for <a className='text-lime-600'>Misskey</a></p>
       <br></br>
 
-      <main className='w-2/3 bg-slate-200/60 mx-auto rounded-lg opacity-90 p-5 overflow-x-hidden' style={{ minWidth: 280 }}>
+      <div className='w-2/3 bg-slate-200/60 mx-auto rounded-lg opacity-90 p-5 overflow-x-hidden' style={{ minWidth: 280 }}>
         <ReactEmbedGist titleClass='hidden' loadingClass='hidden' gist='NarixHine/6451fe18c4924fa55c3102abd8083cdc'></ReactEmbedGist>
         <hr className='my-5 border-purple-400'></hr>
         <iframe src='/note/misskey.cloud/9grjhkquhi' width={'100%'} height={360}></iframe>
         <br></br>
         <iframe src='/timeline/misskey.cloud/9cxdmiu7h5' width={'100%'} height={1000}></iframe>
-      </main>
+      </div>
       <br></br>
 
       <footer className='text-center text-slate-700 align-middle'>— Made by <a target='_blank' rel='noreferrer' href='https://github.com/NarixHine' className='italic'>Narix Hine <Image src='/nh.png' className='align-middle mx-1' quality={100} width={22} height={25} alt='Narix Hine'></Image></a> —</footer>
-    </Div100vh>
+    </main>
   )
 }
