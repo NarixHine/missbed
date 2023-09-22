42bc1ab62ac21bd10e0ec0de1addf7a44878280c
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2023-09-22 23:40:20 +0800

    color-scheme supported

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
+import { useEffect } from 'react'
+import usePrefersColorScheme from 'use-prefers-color-scheme'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const mode = usePrefersColorScheme()
+  useEffect(() => {
+    document.documentElement.setAttribute('style', `color-scheme: ${mode === 'light' ? 'light' : 'dark'};`)
+  }, [mode])
   return (
     <>
       <Head>
