40ec7cb3aa0125d826f032981296a6318b8b9d8a
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2023-10-02 23:14:20 +0800

    Bug Fixes

--- app/timeline/[[...slug]]/loading.tsx
+++ app/timeline/[[...slug]]/loading.tsx
@@ -4,9 +4,9 @@ export default function EmbeddableTimelineLoading() {
     return (
         <>
             <Skeleton />
-            <div className='w-3 h-3 bg-stone-50 dark:bg-slate-900 mx-auto'></div>
+            <div className='w-3 h-3 bg-stone-50 dark:bg-slate-900 mx-auto animate-pulse'></div>
             <Skeleton />
-            <div className='w-3 h-3 bg-stone-50 dark:bg-slate-900 mx-auto'></div>
+            <div className='w-3 h-3 bg-stone-50 dark:bg-slate-900 mx-auto animate-pulse'></div>
             <Skeleton />
         </>
     )
